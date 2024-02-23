3802ee6841839e2713a7147dc50a34658c4378ee
SamyakBK <samyakbk.bsc23@rvu.edu.in>
2024-02-23 14:01:52 +0530

    test

---
+++ set01/practice.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<math.h>
+
+int input_side();
+int check_scalene(int a, int b, int c);
+void output(int a, int b, int c, int isscalene);
+
+int main()
+{
+  int a = input_side();
+  int b = input_side();
+  int c = input_side();
+  int isscalene = check_scalene(a, b, c);
+  output(a, b, c, isscalene);
+  return 0;
+}
+
+int input_side()
+{
+  int x;
+  printf("Enter a number : ");
+  scanf("%d", &x);
+  return x;
+}
+
+int check_scalene(int a, int b, int c)
+{
+  if(a!=b && a!=c && b!=c)
+  {
+    return 0;
+  }
+  else
+  {
+    return 1;
+  }
+}
+void output(int a, int b, int c, int isscalene)
+{
+  if (isscalene==0)
+  {
+    printf("The triangle with sides %d, %d and %d is scalene", a, b , c);
+  }
+  else if (isscalene==1)
+  {
+    printf("The triangle with sides %d, %d and %d is not scalene\n", a ,b ,c);
+  }
+}
--- set01/problem03.c
+++ set01/problem03.c
@@ -1,29 +1,35 @@
-#include <stdio.h>
+#include<stdio.h>
+
 int input();
 int add(int a, int b);
 void output(int a, int b, int sum);
-int main()
+
+int  main()
 {
-    int a, b, sum;
-    a = input();
-    b = input();
-    add=sum(a, b);
-    output(a, b, sum);
+  int a, b, sum;
+  a = input();
+  b = input();
+  sum = add(a, b);
+  output(a, b, sum);
+  return 0;
 }
+
 int input()
 {
-    int a,b;
-    printf("Enter the value of a and b: ");
-    scanf("%d %d", &a &b);
-    return 0;
+  int x;
+  printf("Enter the value of a and b : \n");
+  scanf("%d", &x);
+  return x;
 }
+
 int add(int a, int b)
 {
-    
-    sum = a + b;
-    return sum;
+  int sum=0;
+  sum = a + b;
+  return sum;
 }
+
 void output(int a, int b, int sum)
 {
-    printf("The sum of %d and %d is %d\n", a, b, sum);
+  printf("The sum of %d and %d is : %d\n",a, b, sum);
 }
