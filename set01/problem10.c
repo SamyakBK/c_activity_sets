8846943d35394fbb4f1f7b101d16ca1acf8ba35c
SamyakBK <samyakbk.bsc23@rvu.edu.in>
2024-02-24 22:16:58 +0530

    test

--- set01/problem10.c
+++ set01/problem10.c
@@ -1,8 +1,46 @@
 #include<stdio.h>
-void input_two_strings(char *string1, char *string2);
-int stringcompare(char *string1, char *string2);
+
+char input_strings(char *string1, char *string2);
+char compare_the_strings(char *string1, char *string2);
 void output(char *string1, char *string2, int result);
+
 int main()
 {
-    
+  char string1[100],string2[100];
+  input_strings(string1, string2);
+  int result = compare_the_strings(string1, string2);
+  output(string1, string2, result);
+  return 0;
+}
+
+char input_strings(char *string1, char *string2)
+{
+  printf("Enter the first string : \n");
+  scanf("%s", string1);
+  printf("Enter the second string : \n");
+  scanf("%s", string2);
+  return 0;
+
+}
+char compare_the_strings(char *string1, char *string2)
+{
+  int i=0;
+  for (i=0 ; string1[i]!='\0' && string1[i]==string2[i] ; i++); 
+  return string1[i] - string2[i];
+
+}
+void output(char *string1, char *string2, int result)
+{
+  if(result > 0)
+  {
+    printf("%s is Greater than %s", string1,string2);
+  }
+  else if(result < 0)
+  {
+    printf("%s is Greater than %s", string2,string1);
+  }
+  else if(result == 0)
+  {
+    printf("%s is equal to %s",string1,string2);
+  }
 }
