74763a799d9501b5e1f2abe057a37cd98562c925
SamyakBK <samyakbk.bsc23@rvu.edu.in>
2024-02-27 09:08:51 +0530

    test

--- set01/practice.c
+++ set01/practice.c
@@ -45,3 +45,57 @@ void output(int a, int b, int c, int isscalene)
     printf("The triangle with sides %d, %d and %d is not scalene\n", a ,b ,c);
   }
 }
+
+
+
+
+// 03] Write a program find whether a number is a composite number
+//  A Composite number has more than 2 factors.
+
+#include <stdio.h>
+
+int input_number();
+int is_composite(int n);
+void output(int n, int result);
+
+int main()
+{
+  int n = input_number();
+  int result = is_composite(n);
+  output(n, result);
+  return 0;
+}
+
+int input_number()
+{
+  int n;
+  printf("Enter the value of n : ");
+  scanf("%d", &n);
+  return n;
+}
+
+int is_composite(int n)
+{
+  for (int i = 2; i <= n / 2; i++)
+  {
+    if (n % i == 0)
+    {
+      return 0;
+    }
+    else
+    {
+      return 1;
+    }
+  }
+}
+void output(int n, int result)
+{
+  if (result == 0)
+  {
+    printf("%d is a composite number", n);
+  }
+  else
+  {
+    printf("%d is not a composite number", n);
+  }
+}
