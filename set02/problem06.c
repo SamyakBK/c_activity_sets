e8183341e4fc79ae9fb94c8c9c5ddcbdbfd6a894
SamyakBK <samyakbk.bsc23@rvu.edu.in>
2024-02-29 09:29:08 +0530

    test

---
+++ set02/problem06.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<string.h>
+void input_string(char *a);
+void str_reverse(char *str, char *rev_str);
+void output(char *a, char *reverse_a);
+int main(){
+    char str[100],reverse_str[100];
+    input_string(str);
+    str_reverse(str,reverse_str);
+    output(str,reverse_str);
+}
+void input_string(char *a)
+{
+    printf("Enter the value:\n");
+    scanf("%s",a);
+}
+void str_reverse(char *str, char *rev_str)
+{
+    int len;
+    for(len=0;str[len] !='\0';len++);
+    for(int i = len - 1, j=0;i>=0; i--,i++)
+    {
+        rev_str[j] = str[i];
+    }
+    rev_str[len] = '\0';
+}
+void output(char *a, char *reverse_a)
+{
+    printf("String before reversed : %s\n",a);
+    printf("Reversed String: %s\n",reverse_a);
+}
