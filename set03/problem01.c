e8659e1eba915b7cd0a9b2dadd5d566007d73c20
SamyakBK <samyakbk.bsc23@rvu.edu.in>
2024-03-04 14:24:12 +0530

    test

--- set03/problem01.c
+++ set03/problem01.c
@@ -14,7 +14,7 @@ void input(float *x1, float *y1, float *x2, float *y2){
     printf("Enter the co-ordinate of the first point :\n");
     scanf("%f %f",x1,y1);
     printf("Enter the co-ordinate of the second point : \n");
-  scanf("%f %f", x2, y2);
+    scanf("%f %f", x2, y2);
 }
 float find_distance(float x1, float y1, float x2, float y2){
     float distance = 0;
---
+++ set03/problem04.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int input();
+int find_fibo(int n);
+void output(int n, int fibo);
+int main(){
+    int n = input();
+    int fibo = find_fibo(n);
+    output(n,fibo);
+    return 0;
+}
+int input(){
+    int n;
+    printf("Enter the value of n:");
+    scanf("%d",&n);
+    return n;
+}
+int find_fibo(int n){
+    if(n<=1)
+    {
+        return n;
+    }
+    else{
+        int a =0,b=1,temp;
+        for(int i=2;i<=n;i++){
+            temp = a+b;
+            a=b;
+            b = temp;
+        }
+        return b;
+    }
+}
+void output(int n, int fibo)
+{
+  printf("fibo(%d) = %d\n", n, fibo);
+}
